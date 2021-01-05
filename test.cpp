92e0ea0698c851d73994418044eda77b3afe2ce4
elmerzhh <elmer_zhou@outlook.com>
2021-01-05 20:37:11 +0800

    lll

--- test.cpp
+++ test.cpp
@@ -3,5 +3,14 @@
 int main() {
     std::cout << "Hello World" << std::endl;
     std::cout << "this is used to protice my vim skill" << std::endl;
+
+
+
+
+
+
+
+
+    system("pause");
     return 0;
 }
